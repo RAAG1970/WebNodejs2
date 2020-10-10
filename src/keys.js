31c4bd8768009a8ae798d2c23795ae9d0c84a495
Rodrigo.Alvarez <ralvarezgallardo@gmail.com>
2020-10-10 15:29:41 -0300

    Commit Segundo

---
+++ src/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: '104.156.48.38',
+        user: 'juegaco1_dbuser',
+        password: 'Pololo2009()',
+        database: 'juegaco1_dblinks'
+    }
+
+};
